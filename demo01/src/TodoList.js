cbd64504cc4c7002dc02fa4d3c28c0fd68d3b4b7
quadrant26 <quadrant26@163.com>
2019-07-23 17:10:10 +0800

    TodoList component

--- demo01/src/TodoList.js
+++ demo01/src/TodoList.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+
+const data = [
+    '早8点开晨会，分配今天的开发工作',
+    '早9点和项目经理作开发需求讨论会',
+    '晚5:30对今日代码进行review'
+]
 
 class TodoList extends Component {
     render() { 
         return (
-            <div>Hello World!</div>
+            <div>
+                <div style={{margin: '10px'}}>
+                    <Input placeholder="Write somgthings" style={{width: '250px'}} />
+                    <Button type="primary">添加</Button>
+                </div>
+                <div style={{margin: '10px', width: '300px'}}>
+                    <List 
+                        bordered
+                        dataSource={data}
+                        renderItem={item => (<List.Item>{item}</List.Item>)}
+                    />
+                </div>
+            </div>
         );
     }
 }
