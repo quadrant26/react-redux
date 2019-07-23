6bb0acea3e84b39166d4222f980510b01da79ee3
quadrant26 <quadrant26@163.com>
2019-07-23 17:16:50 +0800

    use state

--- demo01/src/TodoList.js
+++ demo01/src/TodoList.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
-
-const data = [
-    '早8点开晨会，分配今天的开发工作',
-    '早9点和项目经理作开发需求讨论会',
-    '晚5:30对今日代码进行review'
-]
+import store from './store/index'
 
 class TodoList extends Component {
+
+    constructor(props){
+        super(props)
+        console.log( store.getState() )
+        this.state = store.getState()
+    }
     render() { 
         return (
             <div>
                 <div style={{margin: '10px'}}>
-                    <Input placeholder="Write somgthings" style={{width: '250px'}} />
+                    <Input placeholder={this.state.inputValue} style={{width: '250px'}} />
                     <Button type="primary">添加</Button>
                 </div>
                 <div style={{margin: '10px', width: '300px'}}>
                     <List 
                         bordered
-                        dataSource={data}
+                        dataSource={this.state.list}
                         renderItem={item => (<List.Item>{item}</List.Item>)}
                     />
                 </div>
