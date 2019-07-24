47469ef4687a2bd22787115603f9ba2319e13a01
quadrant26 <quadrant26@163.com>
2019-07-24 17:21:46 +0800

    组件UI和业务逻辑拆分

--- demo01/src/TodoList.js
+++ demo01/src/TodoList.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
-import { Input, Button, List } from 'antd'
 import store from './store/index'
-import {CHANGE_INPUT, ADD_ITEM, REMOVE_ITEM} from './store/actionType'
 import { changeInputAction, addItemAction, removeItemAction } from './store/actionCreators'
+import TodoListUI from './TodoListUI'
 
 class TodoList extends Component {
 
@@ -14,29 +13,20 @@ class TodoList extends Component {
         this.changeInputValue = this.changeInputValue.bind(this)
         this.storeChange = this.storeChange.bind(this)
         this.clickBtn = this.clickBtn.bind(this)
+        this.deleteItem = this.deleteItem.bind(this)
         store.subscribe(this.storeChange) // 订阅Redux的状态
 
     }
 
     render() { 
         return (
-            <div>
-                <div style={{margin: '10px'}}>
-                    <Input 
-                        placeholder={this.state.inputValue} 
-                        style={{width: '250px'}}
-                        onChange={this.changeInputValue}
-                    />
-                    <Button type="primary" onClick={this.clickBtn}>添加</Button>
-                </div>
-                <div style={{margin: '10px', width: '300px'}}>
-                    <List 
-                        bordered
-                        dataSource={this.state.list}
-                        renderItem={(item, index) => (<List.Item onClick={this.deleteItem.bind(this, index)}>{item}</List.Item>)}
-                    />
-                </div>
-            </div>
+            <TodoListUI 
+                inputValue = {this.state.inputValue}
+                changeInputValue = {this.changeInputValue}
+                clickBtn={this.clickBtn}
+                list={this.state.list}
+                deleteItem = {this.deleteItem}
+            />
         );
     }
     
---
+++ demo01/src/TodoListUI.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Input, Button, List } from 'antd'
+
+class TodoListUI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+
+        }
+    }
+    render() { 
+        return ( 
+            <div>
+                <div style={{margin: '10px'}}>
+                    <Input 
+                        placeholder={this.props.inputValue} 
+                        style={{width: '250px'}}
+                        onChange={this.props.changeInputValue}
+                        value={this.props.inputValue}
+                    />
+                    <Button type="primary" onClick={this.props.clickBtn}>添加</Button>
+                </div>
+                <div style={{margin: '10px', width: '300px'}}>
+                    <List 
+                        bordered
+                        dataSource={this.props.list}
+                        renderItem={(item, index) => (<List.Item onClick={ (index)=>{this.props.deleteItem(index)}}>{item}</List.Item>)}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+ 
+export default TodoListUI;
