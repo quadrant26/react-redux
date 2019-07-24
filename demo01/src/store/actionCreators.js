07ae8467ed7fa6eb92cdfe06e0f5a29e00d9d803
quadrant26 <quadrant26@163.com>
2019-07-24 18:30:09 +0800

    getTodoList axios 获取数据 箭头函数传参 dispatch

--- demo01/src/store/actionCreators.js
+++ demo01/src/store/actionCreators.js
@@ -1,4 +1,5 @@
 import {CHANGE_INPUT, ADD_ITEM, REMOVE_ITEM, GET_LIST} from './actionType'
+import axios from 'axios'
 
 export const changeInputAction = (value) => ({
     type: CHANGE_INPUT,
@@ -18,3 +19,17 @@ export const getListAction = (data) => ({
     type: GET_LIST,
     data
 })
+
+export const getTodoList = () => {
+    return (dispatch) => {
+        axios.get('https://www.easy-mock.com/mock/5cff5255ae06cd5f4fc7e053/reactdemo01/getList')
+        .then( (res) => {
+            const data = res.data.data
+            const action = getListAction(data)
+            dispatch(action)
+        } )
+        .catch( (error) => {
+            console.log(error)
+        })
+    }
+}
