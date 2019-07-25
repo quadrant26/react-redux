fa7683a21830ce3aeecee65e468d21f92a5f9a34
quadrant26 <quadrant26@163.com>
2019-07-25 16:28:08 +0800

    use saga createSagaMiddleware

--- demo01/src/store/index.js
+++ demo01/src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducer'
-import thunk from 'redux-thunk'
+import createSagaMiddleware  from 'redux-saga'
+import mySagas from './sagas.js'
+// import thunk from 'redux-thunk'
 
 // 无法使用 redux-devtools
 // const store = createStore(
@@ -8,12 +10,22 @@ import thunk from 'redux-thunk'
 //     applyMiddleware(thunk)
 // )
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
-const enhancers = composeEnhancers(applyMiddleware(thunk))
+// 使用 redux-thunk
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+// const enhancers = composeEnhancers(applyMiddleware(thunk))
 
+// const store = createStore(
+//     reducer, 
+//     enhancers
+// )
+
+const sagaMiddleware = createSagaMiddleware()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+const enhancers = composeEnhancers(applyMiddleware(sagaMiddleware))
 const store = createStore(
     reducer, 
     enhancers
 )
+sagaMiddleware.run(mySagas)
 
 export default store
