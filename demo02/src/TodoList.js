baa204bd35020d8055062701253145dea8ecc532
quadrant26 <quadrant26@163.com>
2019-07-25 17:03:23 +0800

    use Provider and connect

--- demo02/src/TodoList.js
+++ demo02/src/TodoList.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
+import store from './store'
+import { connect } from 'react-redux'
 
 class TodoList extends Component {
+    constructor(props){
+        super(props)
+        this.state = store.getState()
+    }
+
     render() { 
         return (
-            <div>Kkang</div>
+            <div>
+                <div>
+                    <input type="text" value={this.props.inputValue} onChange={this.inputChange.bind(this)} />
+                    <button>提交</button>
+                </div>
+                <ul>
+                    <li>kang</li>
+                </ul>
+            </div>
         );
     }
+
+    // inputChange (e){
+    //     console.log(e.target.value)
+    // }
 }
- 
-export default TodoList;
+
+const stateToProps = (state) => {
+    return {
+        inputValue: state.inputValue
+    }
+}
+
+// const dispatchToProps = (dispatch) => {
+//     return {
+//         inputChange(e){
+//             console.log(e.target.value)
+//         }
+//     }
+// }
+
+export default connect(stateToProps, null)(TodoList)
