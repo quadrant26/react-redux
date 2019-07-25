9df5585c3337a252315aaeaf3d2d6128ba680493
quadrant26 <quadrant26@163.com>
2019-07-25 16:40:15 +0800

    react-redux project init

---
+++ demo02/src/TodoList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+    render() { 
+        return (
+            <div>Kkang</div>
+        );
+    }
+}
+ 
+export default TodoList;
